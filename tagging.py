01a5991d6d8dca899184826b1013a117b8bdb70c
Karthick Arya <karya2@ncsu.edu>
2019-11-01 17:39:05 -0400

    Modify word identification

--- tagging.py
+++ tagging.py
@@ -4,9 +4,11 @@ from nltk.corpus import state_union
 import spacy
 import pandas as pd
 import nltk
+import pickle
 import numpy as np
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from sklearn.ensemble import RandomForestClassifier
+import random
 # nltk.download('punkt')
 # nltk.download('averaged_perceptron_tagger')
 # nltk.download('state_union')
@@ -42,51 +44,30 @@ def sentiment(s):
 def POS_Tagging():
     try:
         df = pd.read_csv('zomatoscore.csv')
-        neg = df['neg_score']
-        pos = df['pos_score']
         reviews = df['reviews_list']
+        word_list_col = []        
         for i in range(0,reviews.size):
             words = nltk.word_tokenize(str(reviews.loc[i]))
             tagged = nltk.pos_tag(words)
             key_entities = list(filter(lambda word: word[1]=='JJ' or word[1]=='JJR' or word[1]=='JJS' or word[1]=='VB' or word[1]=='VBD' or word[1]=='VBG' or word[1]=='VBN' or word[1]=='VBP' or word[1]=='VBZ', tagged))
-            word1 = ''
-            word2 = ''
-            score1=0.0
-            score2=0.0
-            if(neg.loc[i]<pos.loc[i]):
-                # s = str(review.loc[i])
-                # wlist = s.split()
-                for entity in key_entities:
-                    f = sentiment(entity[0])
-                    if(score1<=f[2]*100):
-                        score1=f[2]*100
-                        word1 = entity[0]
-                        
-                    if(score2<=f[2]*100 and score2!=score1):
-                        score2=f[2]*100
-                        word2 = entity[0]
-                #df.loc[i,'Max_pos_score'] = maxscore
-                
-            elif(neg.loc[i]>=pos.loc[i]):
-                for entity in key_entities:
-                    f = sentiment(entity[0])
-                    if(score1<=f[0]*100):
-                        score1=f[0]*100
-                        word1 = entity[0]
-
-                    if(score2<=f[0]*100 and score2!=score1):
-                        score2=f[0]*100
-                        word2 = entity[0]
-                #df.loc[i,'Max_neg_score'] = minscore
-
-            df.loc[i,'Max_contributing_word'] = word1
-            df.loc[i,'Secondmax_contributing_word'] = word2
-
+            word_list = random.choices(key_entities, k=int(len(key_entities)/2))
+            for i in range(0,len(word_list)):
+                word_list[i] = word_list[i][0]
+            # score1=0.0
+            # score2=0.0
+            # for i in range(len(word_list)):
+            #     f = sentiment(word_list[i][0])
+            #     temp = word_list[i]
+            #     word_list[i] = (word_list[i][0], f[3])
+            word_list_col.append(word_list)
+            # df.loc[i,'Secondmax_contributing_word'] = word2
+        df['word_list'] = word_list_col
         df.to_csv('zomatoword.csv')
-        df = pd.read_csv('zomatoword.csv')
-        print(df)  
-
-    except exception as e:
+        # df = pd.read_csv('zomatoword.csv')
+        # print(df)  
+        with open("data/tagging.pkl", "wb") as fp:
+            pickle.dump(df, fp)
+    except Exception as e:
         print(str(e))                
 
 # sentences = state_union.raw("2005-GWBUsh.txt").split('\n')
